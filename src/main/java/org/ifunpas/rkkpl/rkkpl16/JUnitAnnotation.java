975a920c6742d308332c0e098e4232748f4e4963
rizqon <rizqon.sidik.maulana@gmail.com>
2016-04-22 16:30:52 +0700

    commit hapus sout

---
+++ src/main/java/org/ifunpas/rkkpl/rkkpl16/JUnitAnnotation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ifunpas.rkkpl.rkkpl16;
+
+/**
+ *
+ * @author SB-604-21
+ */
+public class JUnitAnnotation {
+    
+}
---
+++ src/main/java/org/ifunpas/rkkpl/rkkpl16/MahasiswaMain.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ifunpas.rkkpl.rkkpl16;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+//import org.junit.runner.JUnitCore;
+//import org.junit.runner.Result;
+//import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+/**
+ *
+ * @author SB-604-21
+ */
+public class MahasiswaMain {
+
+    Mahasiswa mhs;
+
+    @Before
+    public void mulaiTest() {
+        mhs = new Mahasiswa();
+        System.out.println("mulai test");
+    }
+
+    @Test
+    public void setNrpTest(String nrp) {
+        mhs.setNrp(nrp);
+        assertNotNull("seharusnya tidak null", mhs.getNrp());
+    }
+
+    @Test
+    public void setNamaTest(String nama) {
+        mhs.setNama(nama);
+        assertNotNull("seharusnya tidak null", mhs.getNrp());
+    }
+    
+    @After
+    public void selesaiTest(){
+        System.out.println("selesai test");
+    }
+
+    
+//    public static void main(String[] args) {
+//        Result result = JUnitCore.runClasses(JUnitAnnotation.class);
+//        for (Failure failure : result.getFailures()) {
+//            System.out.println(failure.toString());
+//        }
+//        System.out.println(result.wasSuccessful());
+//    }
+}
